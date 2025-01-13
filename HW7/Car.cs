987c6bf7c3622f50a95b1379897be5edba54c837
Nibicu <Wams9119@Gmail.com>
2025-01-14 00:16:30 +0700

    1

---
+++ HW7/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW7
+{
+    public class Car : CarBase
+    {
+        public override string Diller { get; } = "Ford";
+        public override string Model { get; } = "Focus";
+        public override string Color { get; } = "Red";
+        public override decimal Price { get; } = 25000.00m;
+
+        public Car(string diller, string model, string color, decimal price)
+        {
+            Diller = diller;
+            Model = model;
+            Color = color;
+            Price = price;
+        }
+
+        public Car()
+        {
+
+        }
+    }
+}
---
+++ HW7/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HW7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+
+            Console.WriteLine($"Производитель: {car1.Diller}");
+            Console.WriteLine($"Модель: {car1.Model}");
+            Console.WriteLine($"Цвет: {car1.Color}");
+            Console.WriteLine($"Цена: {car1.Price}");
+
+            car1.AutoStart();
+            car1.Geolocation();
+            car1.CruiseСontrol();
+            car1.TurnOnTheDisco();
+
+        }
+    }
+}
